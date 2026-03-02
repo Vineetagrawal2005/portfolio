35173f0812421444e5f978dbdb0d1323de772220
Vineetagrawal2306 <vineetagrawal2306@gmail.com>
2026-03-02 23:19:36 +0530

    Some Minor Changes

--- Responsive Portfolio Website Starter/script.js
+++ Responsive Portfolio Website Starter/script.js
@@ -17,3 +17,54 @@ modal.onclick = (e) => {
   }
 };
 
+const jobTitles = [
+  "Frontend Developer",
+  "Flutter Developer",
+  "RTL Developer",
+  "Embedded Developer"
+];
+let jobIndex = 0;
+const jobSpan = document.getElementById('job-title');
+
+function typeText(text, callback) {
+  let i = 0;
+  jobSpan.textContent = "";
+  const typer = setInterval(() => {
+    jobSpan.textContent += text.charAt(i);
+    i++;
+    if (i >= text.length) {
+      clearInterval(typer);
+      if (callback) callback();
+    }
+  }, 100); 
+}
+
+function deleteText(callback) {
+  let text = jobSpan.textContent;
+  let i = text.length;
+  const deleter = setInterval(() => {
+    jobSpan.textContent = text.substring(0, i - 1);
+    i--;
+    if (i <= 0) {
+      clearInterval(deleter);
+      if (callback) callback();
+    }
+  }, 50);
+}
+
+function startTypingCycle() {
+  if (!jobSpan) return;
+  typeText(jobTitles[jobIndex], () => {
+    setTimeout(() => {
+      deleteText(() => {
+        jobIndex = (jobIndex + 1) % jobTitles.length;
+        startTypingCycle();
+      });
+    }, 1000);
+  });
+}
+
+if (jobSpan) {
+  startTypingCycle();
+}
+
